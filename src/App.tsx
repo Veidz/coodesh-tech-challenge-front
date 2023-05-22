3050db56c29e99da0bbc94f0f8b2ca28c64199c3
Barthmosz <joaovictorveidz@gmail.com>
2023-05-21 23:57:07 -0300

    feat: get and map transactions from the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { Transaction } from './contracts/transaction'
 import api from './api/service'
 import './App.css'
 
 function App() {
   const [error, setError] = useState('')
+  const [transactions, setTransactions] = useState([])
 
   const handleFileUpload = async (event: any) => {
     const file = event.target.files[0];
@@ -17,18 +19,48 @@ function App() {
       .catch(function (error) {
         if (error.response) {
           setError(error.response.data.errors[0])
+          setTransactions([])
         }
       })
   }
 
+  const getData = async () => {
+    setTransactions([])
+    setError('')
+    api
+      .get("https://localhost:7192/Transaction")
+      .then((response) => setTransactions(response.data.data))
+      .catch((error) => console.error(error))
+  }
+
   return (
     <div className='container'>
-      <form className='form'>
-        <input type="file" accept='.txt' name='file' onChange={handleFileUpload} />
+      <div className='header'>
+        <form className='form'>
+          <input type="file" accept='.txt' name='file' onChange={handleFileUpload} />
+          {
+            error && <p className='error'>{ error }</p>
+          }
+        </form>
+
+        <div>
+          <button onClick={() => getData()}>Get Data</button>
+        </div>
+      </div>
+
+      <div className='list-container'>
         {
-          error && <p className='error'>{ error }</p>
+          transactions.map((transaction: Transaction) => (
+            <div className='transaction' key={transaction.id}>
+              <h1>Id: {transaction.id}</h1>
+              <p>Type: {transaction.type}</p>
+              <p>Product: {transaction.product}</p>
+              <p>Price: R${transaction.price}</p>
+              <p>Seller: {transaction.seller}</p>
+            </div>
+          ))
         }
-      </form>
+      </div>
     </div>
   )
 }
